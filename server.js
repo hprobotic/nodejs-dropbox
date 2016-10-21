1cc92f759c1f5f68da3b01c41be312f89dcefbfa
JohnP <hprobotic@gmail.com>
2016-10-22 01:06:45 +0700

    Update app.get and response data

--- server.js
+++ server.js
@@ -18,8 +18,16 @@ if(NODE_ENV == 'development') {
 
 app.listen(PORT, ()=> console.log(`LSNING @ http://127.0.0.1:${PORT}`))
 
-
-app.get('*', (req, res, next) => {
+// GET
+app.get('*', setResponseHeaders, setResponseMetaData, (req, res, next) => {
+    if(res.body) {
+        res.json(res.body)
+        return
+    } try {
+        fs.createReadStream(req.filePath).pipe(res)
+    } catch(e) {
+        console.log(`${e.stack} : ${e.message}`)
+    }
 })
 
 app.head('*', (req, res, next) => {
@@ -39,11 +47,38 @@ app.delete('*', (req, res, next) => {
 })
 
 
+// Extend request and response data
 
+function setResponseMetaData(req, res, next) {
+    req.filePath = path.resolve(path.join(ROOT_DIR, req.url))
+    let filePath = req.filePath
+    fs.promise.stat(filePath)
+        .then(
+        stat => req.stat = stat, () => req.stat = null
+        ).nodeify(next)
 
+}
 
+function setResponseHeaders(req, res, next) {
+    nodeify(async ()=> {
+        let filePath = req.filePath
+        console.log(`File path: ${filePath}`)
 
+        let stat = req.stat
+        if (stat.isDirectory()) {
+            let files = await fs.promise.readdir(filePath)
+            res.body = JSON.stringify(files)
+            res.setHeader('Content-Length', res.body.length)
+            res.setHeader('Content-Type', 'application/json')
+            return
+        } else {
+            res.setHeader('Content-Length', stat.size)
+            res.setHeader('Content-Type', mime.contentType(path.extname(path)))
+        }
 
+    }, next)
+
+}
 
 
 
